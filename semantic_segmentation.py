35874ff0a63078cf0775d4ca21e24c22bc6e8c09
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-10-28 22:04:58 +0200

    feat: implemented basic segmentation model

--- semantic_segmentation.py
+++ semantic_segmentation.py
@@ -1,4 +1,20 @@
-"""Perform semantic segmentation on CityScapes dataset using EfficientViTB3 model."""
+import timm
+import torch.nn as nn
+
+
+class SegmentationModel(nn.Module):
+    """The model for semantic segmentation using EfficientViTB3 as backbone"""
+
+    def __init__(self, num_classes) -> None:
+        super(SegmentationModel, self).__init__()
+        self.backbone = timm.create_model("efficientvit_b3", pretrained=True)
+        self.classifier = nn.Conv2d(
+            self.backbone.num_features, num_classes, kernel_size=1
+        )
+
+    def forward(self, x):
+        features = self.backbone(x)
+        return self.classifier(features)
 
 
 def main(): 
