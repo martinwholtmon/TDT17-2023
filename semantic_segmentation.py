40164922b14b63cde289c98974e8f8c7fcb8c62e
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-10-28 22:06:05 +0200

    feat: created function to load the data

--- semantic_segmentation.py
+++ semantic_segmentation.py
@@ -1,5 +1,8 @@
+from pathlib import Path
 import timm
 import torch.nn as nn
+from torchvision import datasets
+from torch.utils.data import DataLoader
 
 
 class SegmentationModel(nn.Module):
@@ -16,6 +19,34 @@ class SegmentationModel(nn.Module):
         features = self.backbone(x)
         return self.classifier(features)
 
+
+def get_dataloaders(batch_size: int = 32) -> tuple[DataLoader, DataLoader]:
+    """Load the dataset and return the dataloaders
+
+    Args:
+        batch_size (int, optional): Size of the batch. Defaults to 32.
+
+    Returns:
+        tuple[DataLoader, DataLoader]: train_loader, test_loader
+    """
+    # root dir
+    data_dir = Path(__file__) / "data" / "cityscapes"
+    print(f"Loading data from {data_dir}")
+
+    # Load data from directory
+    # TODO: Add transforms
+    train_dataset = datasets.Cityscapes(
+        data_dir, split="train", mode="fine", target_type="semantic"
+    )
+    test_dataset = datasets.Cityscapes(
+        data_dir, split="val", mode="fine", target_type="semantic"
+    )
+
+    # Define dataloaders
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
 
 def main(): 
     print("Hello World!")
