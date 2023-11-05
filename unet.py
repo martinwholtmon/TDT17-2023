dbb598e70d49f74026a2365668e13e55f072e708
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-11-05 10:51:20 +0100

    feat: added method to test the implementation

--- unet.py
+++ unet.py
@@ -111,3 +111,16 @@ class UNet(nn.Module):
             self.eval()
         x = self.forward(x)
         return x
+
+
+def test():
+    x = torch.randn((3, 1, 161, 161))
+    model = UNet(in_channels=1, out_channels=1)
+    preds = model(x)
+    print(preds.shape)
+    print(x.shape)
+    assert preds.shape == x.shape
+
+
+if __name__ == "__main__":
+    test()
