d221feb82baaf6ad0f98a89d2b50c53b4f1550b3
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-11-05 10:49:44 +0100

    feat: added conv block

--- unet.py
+++ unet.py
@@ -1,5 +1,48 @@
 import torch
 import torch.nn as nn
+
+
+class DoubleConv(nn.Module):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size=3,
+        padding=1,
+        stride=1,
+        activation=nn.ReLU,
+        norm_layer=nn.BatchNorm2d,
+    ):
+        super(DoubleConv, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=False,
+        )
+        self.conv2 = nn.Conv2d(
+            out_channels,
+            out_channels,
+            kernel_size,
+            stride=stride,
+            padding=padding,
+            bias=False,
+        )
+        self.bn = norm_layer(out_channels)
+        self.act = activation(inplace=True)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn(x)
+        x = self.act(x)
+        x = self.conv2(x)
+        x = self.bn(x)
+        x = self.act(x)
+        return x
+
+
 class UNet(nn.Module):
     def __init__(self, in_channels=3, out_channels=2, features=[64, 128, 256, 512]):
         super(UNet, self).__init__()
