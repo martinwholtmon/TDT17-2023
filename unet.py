c27f558794ddb6b1d7ff3ae60972518e95fbad97
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-11-05 10:50:38 +0100

    feat: forward function

--- unet.py
+++ unet.py
@@ -67,7 +67,34 @@ class UNet(nn.Module):
         # Change the out_channels of the final convolution to num_classes
         self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
         self._init_weight()
+
     def forward(self, x):
+        skip_connections = []
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip_connection = skip_connections[idx // 2]
+
+            if x.shape != skip_connection.shape:
+                x = nn.functional.interpolate(
+                    x,
+                    size=skip_connection.shape[2:],
+                    mode="bilinear",
+                    align_corners=True,
+                )
+
+            concat_skip = torch.cat((skip_connection, x), dim=1)
+            x = self.ups[idx + 1](concat_skip)
+
+        return self.final_conv(x)
+
     def _init_weight(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
