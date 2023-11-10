b747f80500341147c96f9dac0e147d92de968a3f
Martin Wighus Holtmon <martinwh@stud.ntnu.no>
2023-11-10 23:41:46 +0100

    WIP: changed to MetricCollection

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import torch
 import pytorch_lightning as pl
-from torchmetrics import Accuracy, JaccardIndex, FBetaScore
+from torchmetrics import MetricCollection, Accuracy, JaccardIndex, FBetaScore
 
 
 class SegmentationModel(pl.LightningModule):
@@ -10,20 +10,44 @@ class SegmentationModel(pl.LightningModule):
         self.criterion = criterion
         self.optimizer = optimizer
         self.num_classes = num_classes
-
-    def setup(self, stage=None):
-        # Initialize metrics here to ensure they are on the right device
-        device = self.device  # Gets the device model is on
-        self.train_metrics = self._init_metrics(device)
-        self.val_metrics = self._init_metrics(device)
-        self.test_metrics = self._init_metrics(device)
+        metrics = MetricCollection(
+            [
+                Accuracy(
+                    task="multiclass",
+                    num_classes=self.num_classes,
+                    validate_args=True,
+                    ignore_index=None,
+                    average="micro",
+                ),
+                JaccardIndex(
+                    task="multiclass",
+                    threshold=0.5,
+                    num_classes=self.num_classes,
+                    validate_args=True,
+                    ignore_index=None,
+                    average="macro",
+                ),
+                FBetaScore(
+                    task="multiclass",
+                    beta=1.0,
+                    threshold=0.5,
+                    num_classes=self.num_classes,
+                    average="micro",
+                    ignore_index=None,
+                    validate_args=True,
+                ),
+            ],
+            postfix="_micro",
+        )
+        self.train_metrics = metrics.clone(prefix="train_")
+        self.valid_metrics = metrics.clone(prefix="val_")
+        self.test_metrics = metrics.clone(prefix="test_")
 
     def forward(self, x):
         return self.model(x)
 
-    def share_step(self, batch, metrics, stage: str, on_step: bool, on_epoch: bool):
+    def share_step(self, batch, stage: str, on_step: bool, on_epoch: bool):
         x, y = batch
-        y = y.squeeze(1)
 
         # Forward pass
         y_hat = self.forward(x.to(torch.float32))
@@ -31,20 +55,22 @@ class SegmentationModel(pl.LightningModule):
         prob_mask = torch.softmax(y_hat, dim=1)
         prediction = torch.argmax(prob_mask, dim=1)
 
-        # Compute metrics
-        acc_value = metrics["acc"](prediction, y)
-        jaccard_index_value = metrics["jaccard_index"](prediction, y)
-        fbeta_score_value = metrics["fbeta_score"](prediction, y)
-
         # Log metrics
-        metrics_dict = {
-            f"{stage}_loss": loss,
-            f"{stage}_acc": acc_value,
-            f"{stage}_jaccard": jaccard_index_value,
-            f"{stage}_fbeta": fbeta_score_value,
-        }
+        match stage:
+            case "train":
+                output = self.train_metrics(prediction, y)
+            case "val":
+                output = self.valid_metrics(prediction, y)
+            case "test":
+                output = self.test_metrics(prediction, y)
+            case _:
+                raise ValueError(f"Invalid stage: {stage}")
+
+        # Add loss to the output dictionary
+        output[f"{stage}_loss"] = loss
+
         self.log_dict(
-            metrics_dict,
+            output,
             prog_bar=True,
             sync_dist=True,
             on_step=on_step,
@@ -53,19 +79,13 @@ class SegmentationModel(pl.LightningModule):
         return loss
 
     def training_step(self, batch, batch_idx):
-        return self.share_step(
-            batch, self.train_metrics, "train", on_step=True, on_epoch=False
-        )
+        return self.share_step(batch, "train", on_step=True, on_epoch=False)
 
     def validation_step(self, batch, batch_idx):
-        return self.share_step(
-            batch, self.val_metrics, "val", on_step=True, on_epoch=True
-        )
+        return self.share_step(batch, "val", on_step=True, on_epoch=True)
 
     def test_step(self, batch, batch_idx):
-        return self.share_step(
-            batch, self.test_metrics, "test", on_step=True, on_epoch=True
-        )
+        return self.share_step(batch, "test", on_step=True, on_epoch=True)
 
     def predict_step(self, batch, batch_idx, dataloader_idx=None):
         x, y = batch
@@ -76,32 +96,3 @@ class SegmentationModel(pl.LightningModule):
 
     def configure_optimizers(self):
         return self.optimizer
-
-    def _init_metrics(self, device):
-        # Helper function to initialize metrics
-        return {
-            "acc": Accuracy(
-                task="multiclass",
-                num_classes=self.num_classes,
-                validate_args=True,
-                ignore_index=None,
-                average="micro",
-            ).to(device),
-            "jaccard_index": JaccardIndex(
-                task="multiclass",
-                threshold=0.5,
-                num_classes=self.num_classes,
-                validate_args=True,
-                ignore_index=None,
-                average="macro",
-            ).to(device),
-            "fbeta_score": FBetaScore(
-                task="multiclass",
-                beta=1.0,
-                threshold=0.5,
-                num_classes=self.num_classes,
-                average="micro",
-                ignore_index=None,
-                validate_args=True,
-            ).to(device),
-        }
